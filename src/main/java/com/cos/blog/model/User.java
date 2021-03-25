fa7d20004c55c307374a91093f0e494ff47d7a25
breadmk <minvita9999@naver.com>
2021-03-25 19:17:10 +0900

    insertData

--- src/main/java/com/cos/blog/model/User.java
+++ src/main/java/com/cos/blog/model/User.java
@@ -4,11 +4,12 @@ import java.sql.Timestamp;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
-import org.hibernate.annotations.ColumnDefault;
 import org.hibernate.annotations.CreationTimestamp;
 
 import lombok.AllArgsConstructor;
@@ -23,6 +24,7 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor //전체생성자
 @Builder //빌더 패턴
 @Entity //User 클래스가 MySQL에 테이블이 생성이 된다.
+//@DynamicInsert //insert 할때 null 인 필드 제외
 public class User {
 	
 	@Id //Primary key
@@ -30,7 +32,7 @@ public class User {
 	private int id; //시퀀스 , auto_increment;
 	
 	@Column(nullable = false,length = 30)
-	private String usernameString;
+	private String username;
 	
 	@Column(nullable = false,length = 100) //123456 => 해쉬(비밀번호 암호화)
 	private String password;
@@ -38,8 +40,9 @@ public class User {
 	@Column(nullable = false,length = 50)
 	private String email;
 	
-	@ColumnDefault("'user'") //문자라는걸 알려줘야한다.
-	private String role; // Enum을 쓰는게 좋다. //admin, user,manager
+//	@ColumnDefault("'user'") //문자라는걸 알려줘야한다.
+	@Enumerated(EnumType.STRING)//DB는 RoleType가 없다. 그래서 붙여주는 어노테이션
+	private RoleType role; // Enum을 쓰는게 좋다. //ADMIN,USER
 	
 	@CreationTimestamp //시간이 자동 입력
 	private Timestamp createDate;
