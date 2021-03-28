0b3e2c5a3c9666ff350d166d03c6aa1a9801c44e
breadmk <minvita9999@naver.com>
2021-03-28 10:10:55 +0900

    스프링시큐리티(로그인)설정

--- src/main/java/com/cos/blog/repository/UserRepository.java
+++ src/main/java/com/cos/blog/repository/UserRepository.java
@@ -1,7 +1,8 @@
 package com.cos.blog.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 
 import com.cos.blog.model.User;
 
@@ -10,6 +11,8 @@ import com.cos.blog.model.User;
 //@repository 생략이 가능.
 public interface UserRepository extends JpaRepository<User,Integer>{
 	
+	// select * from user where username = 1?;
+	Optional<User> findByUsername(String username);
 
 }
 
