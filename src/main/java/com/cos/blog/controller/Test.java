8414cb3bd6a367a7268b373db51880966631a333
breadmk <minvita9999@naver.com>
2021-03-31 22:33:52 +0900

    글삭제하기

--- src/main/java/com/cos/blog/controller/Test.java
+++ src/main/java/com/cos/blog/controller/Test.java
@@ -1,5 +1,7 @@
 package com.cos.blog.controller;
 
+import ch.qos.logback.core.net.SyslogOutputStream;
+
 public class Test {
 
 	public static void main(String[] args) {
@@ -10,6 +12,41 @@ public class Test {
 				sum +=i;
 			}
 		}
-		System.out.println(sum);
-	}
+//		System.out.println(sum);
+		int sum1 = 0;
+		
+		for (int i=1; i<=100; i++) {
+			if(i%3!=0) {
+				sum1 +=i;
+//				System.out.println(i);
+			}
+		}
+//		System.out.println(sum1);
+		int sum2=0;
+//		제가 얼마전 신입면접 보러가서 손코딩 받은 문제가 1부터 1000까지 3이나 5의 배수 합 구하기 였어용!
+		for(int i=1; i<1000; i++) {
+			if(i%3==0 || i%5==0) {
+				sum2 += i;
+			}
+		}
+//		System.out.println(sum2);
+		
+		
+		for(int i=2; i<=9; i++) {
+			for(int j=1; j<=9; j++) {
+//				System.out.println(i+"*"+j+"="+i*j);
+//				System.out.printf("%d*%d=%d\n",i,j,i*j);
+			}
+		}
+		for(int i=1; i<=5; i++) {
+			for(int k=5; k>0; k--) {
+				if(k>i) {
+					System.out.print(" ");
+				}else {
+					System.out.print("*");
+				}
+			}
+			System.out.println();
+		}
+	}	
 }
--- src/main/resources/static/js/board.js
+++ src/main/resources/static/js/board.js
@@ -3,9 +3,9 @@ let index={
 		$("#btn-save").on("click",()=>{ //function(){} , ()=>{} this를 바인딩하기 위해서 사용.
 			this.save();
 		});	
-//		$("#btn-login").on("click",()=>{ //function(){} , ()=>{} this를 바인딩하기 위해서 사용.
-//			this.login();
-//		});	
+		$("#btn-delete").on("click",()=>{ //function(){} , ()=>{} this를 바인딩하기 위해서 사용.
+			this.deleteById(); //delete 가 예약어여서 불가능함.
+		});	
 	},
 	save:function(){
 //		alert("userinit이 호출됨");
@@ -28,6 +28,25 @@ let index={
 			console.log(error);
 			alert("중복 아이디입니다.")
 //			alert(JSON.stringify(error));
+		}); 
+		
+	},
+	
+	deleteById:function(){
+		
+		var id = $("#id").text();
+		
+		$.ajax({
+			type:"delete",
+			url:"/api/board/"+id,
+			dataType:"json" 
+		}).done(function(resp){
+			alert("삭제가 완료 되었습니다");
+			location.href="/";
+		}).fail(function(error){
+			console.log(error);
+//			alert("중복 아이디입니다.")
+			alert(JSON.stringify(error));
 		}); 
 		
 	},
